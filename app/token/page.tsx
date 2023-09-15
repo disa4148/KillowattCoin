be655d0aa1bd1bf7fc012fc37748937e3e3eacf2
Rodion <eclipselucky@gmail.com>
2023-09-15 10:29:05 +0300

    fix(widgets): contact on token page

--- app/token/page.tsx
+++ app/token/page.tsx
@@ -2,6 +2,7 @@ import { Tokenomic } from "../widgets/tokenomic/Tokenomic"
 import ChunkedStatisticsToken from "../widgets/chunkedStatistics/ChunkedStatisticToken"
 import { StatisticApi } from "../widgets/statisticApi/StatisticApi"
 import TokenInfo from "../widgets/tokenInfo/TokenInfo"
+import { Contacts } from "../widgets/contacts/Contacts"
 
 const Token = () => {
   return (
@@ -10,6 +11,7 @@ const Token = () => {
       <Tokenomic/>
       <ChunkedStatisticsToken/>
       <StatisticApi/>
+      <Contacts/>
     </div>
   )
 }
